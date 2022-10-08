44cf8fc64c4f1ab6dd2d0624fc6ba05bd701f0c6
Abdirahman <raaxeyemanka@gmail.com>
2022-10-08 06:13:02 +0300

    added files of the project
    The project is built in HTML CSS and Js

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # AUTO-TYPE-TEXT
 TEXT AUTO-TYPE IN JAVASCRIPT
 This is a small javascript program that shows an auto-typing text on screen
+Lets have a look at the project
---
+++ script.js
@@ -0,0 +1,8 @@
+let typed = new Typed(".auto-input", {
+
+    strings: ["Abdirahman Farah ", "Software Developer", "Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+
+})
